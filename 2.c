3c532b3913e04a4397ef910d94e13c511cc02f03
Amircl <amir.nigmatzyanov.2007@mail.ru>
2025-05-28 12:17:40 +0300

    Create 2.c

---
+++ 2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int blue_balls[5]; // Массив на 5 элементов
+    
+    int size = sizeof(blue_balls) / sizeof(blue_balls[0]);
+    
+    printf("В коробке blue_balls помещается %d шариков.\n", size);
+    
+    return 0;
+}
